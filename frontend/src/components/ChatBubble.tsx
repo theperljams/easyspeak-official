55eeb09489ac4a82ce89281dba067e426c1173c9
Paul Hathaway <paulchathaway@gmail.com>
2023-12-02 16:25:48 -0700

    finish ChatBubble component

---
+++ frontend/src/components/ChatBubble.tsx
@@ -0,0 +1,35 @@
+interface ChatBubbleProps {
+    side: 'left' | 'right';
+    message: string;
+}
+
+const ChatBubble = ({side, message}: ChatBubbleProps) => {
+    return (
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: side === 'left' ? 'flex-start' : 'flex-end',
+            width: '100%',
+        }}>
+            <div
+                style={{
+                    backgroundColor: side === 'left' ? '#007AFF' : '#DCE7F9',
+                    color: side === 'left' ? '#F2F2F2' : '#007AFF',
+                    borderRadius: '20px',
+                    margin: '0',
+                    maxWidth: '70%'
+                }}>
+                <p
+                    style={{
+                        margin: '10px',
+                        padding: '10px 0',
+                        textAlign: 'center',
+                        fontSize: '.5em',
+
+                    }}>{message}</p>
+            </div>
+        </div>
+    );
+}
+
+export default ChatBubble;
