c58e2532a747b5f44b599dad34af937dd186f763
Paul Hathaway <paulchathaway@gmail.com>
2023-12-02 14:23:37 -0700

    init Input component

--- frontend/src/components/MainView.tsx
+++ frontend/src/components/MainView.tsx
@@ -1,15 +1,16 @@
 import styles from './MainView.module.css';
 import Options from "./Options";
+import Input from "./Input";
 
 const MainView = () => {
     return (
             <div className={styles.layout}>
-                <div className={styles.grid_input}>test</div>
-                <div className={styles.grid_conversation}>test</div>
+                <div className={styles.grid_input}><Input/></div>
+                <div className={styles.grid_conversation}></div>
                 <div className={styles.grid_options}>
                     <Options />
                 </div>
-                <div className={styles.grid_responses}>tset</div>
+                <div className={styles.grid_responses}></div>
                 <div className={styles.grid_responsesLabel}>Responses</div>
             </div>
     );
