effc1f80aaf3678cea84f97e46918a2659a5b410
Paul Hathaway <paulchathaway@gmail.com>
2023-12-02 14:56:32 -0700

    create input view

--- frontend/src/components/Input.tsx
+++ frontend/src/components/Input.tsx
@@ -1,7 +1,29 @@
+import Pencil from '../assets/pencil-solid.svg';
+import Send from '../assets/paper-plane-regular.svg';
+import CircleWithIcon from "./CircleWithIcon";
 const Input = () => {
     return (
-        <div>
+        <div style={{
+            display: 'flex',
+            flexDirection: 'row',
+            alignItems: 'center',
+            justifyContent: 'start',
+            width: '100%',
+            height: '75%',
+            gap: '10px',
+            padding: '10px',
 
+        }}>
+            <CircleWithIcon icon={Pencil} />
+            <input type={'text'} style={{
+                fontSize: '.5em',
+                borderRadius: '15px',
+                paddingLeft: '20px',
+                paddingRight: '20px',
+                height: '75%',
+                width: '100%',
+            }}/>
+            <CircleWithIcon icon={Send} />
         </div>
     )
 }
