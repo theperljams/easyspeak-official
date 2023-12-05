46f2003ade4ad94b80474b466ec853a4256de049
Justin McBride <jstnmcbrd@outlook.com>
2023-12-05 01:58:30 -0700

    Improve input bar

--- front-end/src/components/MainView.tsx
+++ front-end/src/components/MainView.tsx
@@ -1,13 +1,15 @@
 import styles from "./MainView.module.css";
 import { Options } from "./Options.jsx";
-import { Input } from "./Input.jsx";
+import { InputBar } from "./input-bar/InputBar.js";
 import { Responses } from "./Responses.jsx";
 import { Conversation } from "./Conversation.jsx";
 
 export function MainView () {
 	return (
 		<div className={styles.layout}>
-			<div className={styles.grid_input}><Input/></div>
+			<div className={styles.grid_input}>
+				<InputBar/>
+			</div>
 			<div className={styles.grid_conversation}>
 				<Conversation />
 			</div>
@@ -17,7 +19,9 @@ export function MainView () {
 			<div className={styles.grid_responses}>
 				<Responses />
 			</div>
-			<div className={styles.grid_responsesLabel}>Responses</div>
+			<div className={styles.grid_responsesLabel}>
+				Responses
+			</div>
 		</div>
 	);
 }
---
+++ front-end/src/components/input-bar/InputButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import styles from "./InputBar.module.css";
+
+interface Props {
+    /** Path to the SVG file */
+    icon: string;
+    clicked?: () => void;
+}
+
+export function InputButton ({ icon, clicked }: Props) {
+	return (
+		<div className={styles.button} onClick={clicked}>
+			<img src={icon} alt="icon" className={styles.button_icon} />
+		</div>
+	);
+}
