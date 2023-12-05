d50b55e3fb5851349f98537f1685c2e274d4eff7
Justin McBride <jstnmcbrd@outlook.com>
2023-12-04 22:56:18 -0700

    Add unit tests with vitest

---
+++ front-end/src/reportWebVitals.test.ts
@@ -0,0 +1,34 @@
+
+import { basename } from "node:path";
+import { describe, expect, test, vi } from "vitest";
+
+import { reportWebVitals } from "./reportWebVitals";
+
+const mockWebVitals = vi.hoisted(() => ({
+	onCLS: vi.fn(),
+	onFID: vi.fn(),
+	onFCP: vi.fn(),
+	onLCP: vi.fn(),
+	onTTFB: vi.fn(),
+}));
+vi.mock("web-vitals", () => mockWebVitals);
+
+describe(basename(import.meta.url), () => {
+	test("registers onPerfEntry callback web-vitals event handlers", () => {
+		const mockOnPerfEntry = vi.fn();
+
+		reportWebVitals(mockOnPerfEntry);
+		expect(mockWebVitals.onCLS).toHaveBeenCalledWith(mockOnPerfEntry);
+		expect(mockWebVitals.onFID).toHaveBeenCalledWith(mockOnPerfEntry);
+		expect(mockWebVitals.onFCP).toHaveBeenCalledWith(mockOnPerfEntry);
+		expect(mockWebVitals.onLCP).toHaveBeenCalledWith(mockOnPerfEntry);
+		expect(mockWebVitals.onTTFB).toHaveBeenCalledWith(mockOnPerfEntry);
+	});
+
+	test("does not throw an error when onPerfEntry is not provided", () => {
+		expect(() => {
+			reportWebVitals();
+		}).not.toThrow();
+	});
+});
+
