4d1408d12db1811bdd7ec2d5af7173811f2785e5
clamguts <pearl.k.hulbert@gmail.com>
2023-12-09 18:55:20 -0700

    props?

--- front-end/src/components/MainView.tsx
+++ front-end/src/components/MainView.tsx
@@ -4,20 +4,29 @@ import { Input } from "./Input.jsx";
 import { Responses } from "./Responses.jsx";
 import { Conversation } from "./Conversation.jsx";
 
-export function MainView () {
-	return (
-		<div className={styles.layout}>
-			<div className={styles.grid_input}><Input/></div>
-			<div className={styles.grid_conversation}>
-				<Conversation />
-			</div>
-			<div className={styles.grid_options}>
-				<Options />
-			</div>
-			<div className={styles.grid_responses}>
-				<Responses />
-			</div>
-			<div className={styles.grid_responsesLabel}>Responses</div>
-		</div>
-	);
-}
+import { useState } from "react";
+
+const MainView = () => {
+  const [listen, setListen] = useState(false);
+  const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+
+  return (
+    <div className={styles.layout}>
+      <div className={styles.grid_input}>
+        <Input />
+      </div>
+      <div className={styles.grid_conversation}>
+        <Conversation />
+      </div>
+      <div className={styles.grid_options}>
+        <Options listen={listen} setListen={setListen} webSocket={webSocket} setWebSocket={setWebSocket}/>
+      </div>
+      <div className={styles.grid_responses}>
+        <Responses />
+      </div>
+      <div className={styles.grid_responsesLabel}>Responses</div>
+    </div>
+  );
+};
+
+export default MainView;
--- front-end/src/components/Options.tsx
+++ front-end/src/components/Options.tsx
@@ -1,101 +1,95 @@
-import { useState } from "react";
-
-import VolumeHigh from "../assets/volume-high-solid.svg";
-import VolumeMute from "../assets/volume-xmark-solid.svg";
 import styles from "./Options.module.css";
 
-export function Options () {
-	const [listen, setListen] = useState(false);
-	const [webSocket, setWebSocket] = useState<WebSocket | null>(null);
+export function Options({ listen, setListen, webSocket, setWebSocket}) {
 	
+  const doListen = async () => {
+    if (listen) {
+      // If already listening, stop listening
+      webSocket?.close();
+    } else {
+      // If not listening, start listening
+      const socket = new WebSocket(WEBSOCKET_URL + "/transcribe");
 
-	const doListen = async () => {
-		if (listen) {
-		  // If already listening, stop listening
-		  webSocket?.close();
-		} else {
-		  // If not listening, start listening
-		  const socket = new WebSocket(WEBSOCKET_URL + '/transcribe');
-	
-		  socket.onopen = (event) => {
-			console.log('WebSocket connection opened:', event);
-		  };
+      socket.onopen = (event) => {
+        console.log("WebSocket connection opened:", event);
+      };
 
-	
-		  socket.onmessage = (event) => {
-			// Handle incoming transcription results
-			const transcriptionResult = event.data;
-			console.log('Transcription Result:', transcriptionResult);
-			setVoiceInput((prevInput) => prevInput + ' ' + transcriptionResult);
-			socket.send('ACK'); // Send acknowledgement back to server
-		  };
-	
-		  socket.onclose = (event) => {
-			if (event.wasClean) {
-			  console.log('WebSocket closed cleanly:', event);
-			} else {
-			  console.log('WebSocket connection closed unexpectedly:', event);
-			}
-		  };
-	
-		  setWebSocket(socket);
-		}
-	  };
-	  
+      socket.onmessage = (event) => {
+        // Handle incoming transcription results
+        const transcriptionResult = event.data;
+        console.log("Transcription Result:", transcriptionResult);
+        setVoiceInput((prevInput) => prevInput + " " + transcriptionResult);
+        socket.send("ACK"); // Send acknowledgement back to server
+      };
 
-	const toggleListen = () => {
-		setListen(!listen);
-		doListen();
-	};
+      socket.onclose = (event) => {
+        if (event.wasClean) {
+          console.log("WebSocket closed cleanly:", event);
+        } else {
+          console.log("WebSocket connection closed unexpectedly:", event);
+        }
+      };
 
-	return (
-		<div style={
-			{
-				display: "flex",
-				flexDirection: "row",
-				alignItems: "left",
-				width: "100%",
-				height: "100%",
-				gap: "10px",
-			}
-		}>
-			<div
-				/* className={`${styles.listen_toggle}`}*/
-				style={{
-					backgroundColor: "#007AFF",
-					display: "flex",
-					flexDirection: "column",
-					justifyContent: "center",
-					padding: "10px",
-					fontSize: ".5em",
-				}}
-				onClick={toggleListen}>
-				<span className={`${styles.vertically_centered_children_row}`}
-					style={{
-						userSelect: "none",
-						minWidth: "250px",
-						cursor: "pointer",
+      setWebSocket(socket);
+    }
+  };
 
-					}}>
-					<img style={{
-						filter: "color(white)",
-					}}
-					src={listen ? VolumeHigh : VolumeMute}
-					width="30px"
-					alt="volume high"/>
-                    &nbsp;
-                    Listen: {listen ? "On" : "Off"}
-				</span>
-			</div>
+  const toggleListen = () => {
+    setListen(!listen);
+    doListen();
+  };
 
-			<div className={styles.vertically_centered_children_column}
-				style={{
-					textAlign: "center",
-					width: "100%",
-				}}>
-                Chat
-			</div>
+  return (
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "row",
+        alignItems: "left",
+        width: "100%",
+        height: "100%",
+        gap: "10px",
+      }}
+    >
+      <div
+        style={{
+          backgroundColor: "#007AFF",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          padding: "10px",
+          fontSize: ".5em",
+        }}
+        onClick={toggleListen}
+      >
+        <span
+          style={{
+            userSelect: "none",
+            minWidth: "250px",
+            cursor: "pointer",
+          }}
+        >
+          <img
+            style={{
+              filter: "color(white)",
+            }}
+            src={listen ? VolumeHigh : VolumeMute}
+            width="30px"
+            alt="volume high"
+          />
+          &nbsp;
+          Listen: {listen ? "On" : "Off"}
+        </span>
+      </div>
 
-		</div>
-	);
+      <div
+        className={styles.vertically_centered_children_column}
+        style={{
+          textAlign: "center",
+          width: "100%",
+        }}
+      >
+        Chat
+      </div>
+    </div>
+  );
 }
