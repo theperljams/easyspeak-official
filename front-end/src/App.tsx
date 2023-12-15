2ea0d44a5f5ffe4b581e9685e993b4b349abb1b6
Justin McBride <jstnmcbrd@outlook.com>
2023-12-15 02:42:15 -0700

    No unnecessary react imports

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 import { MainView } from "./components/MainView.jsx";
 import "./App.css";
 
--- front-end/src/index.tsx
+++ front-end/src/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "./index.css";
@@ -10,9 +10,9 @@ const root = createRoot(
 	document.getElementById("root")!,
 );
 root.render(
-	<React.StrictMode>
+	<StrictMode>
 		<App />
-	</React.StrictMode>,
+	</StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
