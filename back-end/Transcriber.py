7ae1b56860bfcb04cc775444ebefcfcdc5420546
Brendan  Izu <brendanizu@Brendans-MacBook-Pro-4.local>
2023-12-19 16:44:54 -0700

    Add try catch block and type check in trascribe

--- back-end/Transcriber.py
+++ back-end/Transcriber.py
@@ -1,3 +1,4 @@
+import io
 import whisper
 import torch
 from tempfile import NamedTemporaryFile
@@ -21,25 +22,37 @@ class Transcriber:
         print("Model loaded")
 
     def transcribe(self, curr_data: bytes):
-
         if (self.audio_model is None):
             self.load_model()
-
     
         temp_file = NamedTemporaryFile().name
 
-        # Write wav data to the temporary file as bytes.
-        with open(temp_file, 'wb') as f:
-            f.write(curr_data.read())
+        try:
+            if isinstance(curr_data, io.BytesIO):
+                # If curr_data is a BytesIO object, extract bytes.
+                curr_data.seek(0)  # Reset the pointer to the beginning of the BytesIO object
+                data_bytes = curr_data.read()
+            elif isinstance(curr_data, bytes):
+                # If curr_data is already bytes, use it directly.
+                data_bytes = curr_data
+            else:
+                raise TypeError("Invalid data type. Expected bytes or BytesIO object.")
 
-        print("made temp file")
-        # Read the transcription.
-        result = self.audio_model.transcribe(temp_file, fp16=torch.cuda.is_available())
-        os.remove(temp_file)
-        text = result['text'].strip()
-        print("Made text: ", text)
-        return text
-    
-        # else:
-        #     transcription[-1] = text
+            # Create a temporary file and write wav data to it as bytes.
+            with NamedTemporaryFile(delete=False) as temp_file:
+                temp_file.write(data_bytes)
+
+            print("made temp file:", temp_file.name)
+
+            # Read the transcription.
+            result = self.audio_model.transcribe(temp_file.name, fp16=torch.cuda.is_available())
+            os.remove(temp_file.name)
+            text = result['text'].strip()
+            print("Made text:", text)
+            return text
+
+        except Exception as e:
+            # Handle exceptions or specific errors here.
+            print("Error occurred:", e)
+            return None  # Or handle the error according to your requirement.
 
