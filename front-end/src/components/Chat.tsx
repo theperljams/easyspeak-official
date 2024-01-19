2e0e4d7c446a55657514b924ca0a23078eeeeaf3
Justin McBride <jstnmcbrd@outlook.com>
2024-01-18 17:09:38 -0700

    Set scrollbar to bottom when new message added

--- front-end/src/components/Chat.tsx
+++ front-end/src/components/Chat.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+
 import { ChatBubble } from './ChatBubble';
 
 import styles from './Chat.module.css';
@@ -12,12 +14,25 @@ interface Props {
 }
 
 export function Chat({ messages }: Props) {
+	const SCROLLABLE_AREA_ID = 'message-list';
+
+	// Set the scrollbar to the bottom automatically when a new message is added
+	useEffect(() => {
+		const scrollableArea = document.getElementById(SCROLLABLE_AREA_ID);
+		if (scrollableArea) {
+			scrollableArea.scrollTop = scrollableArea.scrollHeight;
+		}
+		else {
+			console.error('Could not find scrollabe area!');
+		}
+	}, [messages]);
+
 	return (
 		<div className={styles.chat}>
 			<div className={styles.titleBar}>
 				Chat
 			</div>
-			<div className={styles.messagesList}>
+			<div id={SCROLLABLE_AREA_ID} className={styles.messagesList}>
 				{messages.map((message, index) => (
 					<ChatBubble key={index} side={message.side} message={message.message} />
 				))}
