5e6f75365224a02973392a1a2e16ec6e2483b6d5
Taylor Bradshaw <Taylor.bradshaw0250@gmail.com>
2024-02-12 18:48:04 -0700

    just need to push for vercel

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -57,5 +57,5 @@ app.post('/tts', async (req, res) => {
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+  console.log(`Server is running on port  ${PORT}`);
 });
