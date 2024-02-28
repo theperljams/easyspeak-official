a74bcc43fc64fbcb5859ab36af999cdb1e7e34db
Brendan  Izu <brendanizu@Brendans-MBP-5.app.byu.edu>
2024-02-28 14:52:01 -0700

    fix training loading message

--- front-end/src/components/ChatWindow.tsx
+++ front-end/src/components/ChatWindow.tsx
@@ -37,7 +37,7 @@ export function ChatWindow({ loading, messages, transcript, introMessages, mode}
         </>}
         {messages.map((message, index) => (
             <ChatBubble mode={mode} key={index} role={message.role} content={message.content} />))}
-        {loading && <ChatBubble mode={mode} role={'user'} content={transcript != '' ? transcript : '...' } />}
+        {loading && <ChatBubble mode={mode} role={'assistant'} content={transcript != '' ? transcript : '...' } />}
         
       </div >
       <div className="footer"/>
