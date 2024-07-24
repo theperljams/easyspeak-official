6709596b72d5a5b2a86098e83e11a90d39f0c0a0
Amy Hulbert <amyhulbert@Amys-MBP.hulbert.local>
2024-07-24 14:43:17 -0700

    capacitor?

---
+++ front-end/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.easyspeak.aac',
+  appName: 'front-end',
+  webDir: 'dist'
+};
+
+export default config;
